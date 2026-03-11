696c955bf58422c04d2b0a6e1c23eb0a68672e9c
martigiron01 <marti.giron01@estudiant.upf.edu>
2026-03-11 19:01:23 +0100

    db acces in .env file

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 import mysql from 'mysql2/promise';
 import fs from 'fs/promises';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const db = await mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'root',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: 'syncode_db'
 });
 
